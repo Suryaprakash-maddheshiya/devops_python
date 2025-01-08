991422ff587b9ae19a7bd1f508c1f8b87d6dc4eb
Suryaprakash <suryaprakash72670@gmail.com>
2025-01-08 23:43:29 +0530

    operators and conditional file upload

---
+++ operatores.py
@@ -0,0 +1,21 @@
+21 * 5     # 105 multi operator
+2 ** 5     # power operator  2 pe power 5
+2 // 5     # floor operator..... it gives only last  1 
+4 / 2     #divide operator...... it gives divident 2.0
+4% 2      #modules operator..... it gives remainder
+# comparison operator 
+# its compare two values and return booleans value....0
+
+#membership operator 
+a = "Pwskills"
+"p" in a
+"p" not in a
+
+#identity operator  // is , is not
+a=3
+b=4
+a is b   # it give false  , a=4, b=a it gives true
+
+#bitwise operator //operations at bit level.  manipulate individuals bits with integar
+
+
